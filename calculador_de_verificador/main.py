ac75781fecfef7f40098d5d24afb7e749bf2e0c6
Deivyson <131066052+DeivRicardoSS@users.noreply.github.com>
2024-03-04 00:28:35 -0300

    Update main.py

--- calculador_de_verificador/main.py
+++ calculador_de_verificador/main.py
@@ -1,5 +1,7 @@
 import os
+from random import randint
 print("carregando funções")
+historico = []
 def limpar():
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -56,6 +58,60 @@ def calcularCnpj(string):
     
     print(f"O seu cnpj completo é:\n{saida}")
 
+def calcularCpfSecret(string):
+    n4 = int(string[:1])
+    n5 = int(string[1:2])
+    n6 = int(string[2:3])
+    n7 = int(string[3:4])
+    n8 = int(string[4:5])
+    n9 = int(string[5:6])
+    n10 = int(string[6:7])
+    n11 = int(string[7:8])
+
+    soma1 = 0
+    soma2 = 0
+
+    tentativas = 0
+    max_tentativas = 90000
+
+    while tentativas <= max_tentativas:
+        n1 = randint(0,9)
+        n2 = randint(0,9)
+        n3 = randint(0,9)
+        soma1 = 11-((n1*10 + n2*9 + n3*8 + n4*7 + n5*6 + n6*5 + n7*4 + n8*3 + n9*2)%11)
+        if soma1 > 9:
+            soma1 = 0
+        
+        soma2 = 11-((n1*11 + n2*10 + n3*9 + n4*8 + n5*7 + n6*6 + n7*5 + n8*4 + n9*3 + n10*2)%11)
+        if soma2 > 9:
+            soma2 = 0
+        
+        if soma2 == n11 and soma1 == n10:
+            saida = f"{n1}{n2}{n3}.{n4}{n5}{n6}.{n7}{n8}{n9}-{soma1}{soma2}"
+            if len(historico) == 0:
+                historico.append(saida)
+            else:
+                ind = 0
+                for x in historico:
+                    
+                    if x != saida:
+                        ind += 1
+                        if ind == len(historico):
+                            historico.append(saida)
+                    else:
+                        ind = 0
+            tentativas += 1
+            return historico
+            
+            
+        
+        
+        
+
+        
+    
+
+    
 
 controle = 0
 while controle == 0:
@@ -79,6 +135,21 @@ while controle == 0:
             calcularCnpj(cnpj)
         else:
             print("entrada inválida")
+    elif controle == 3:
+        limpar()
+        print("Calcular 3 primeiros dígitos do cpf\nDigite o cpf ignorando os 3 primeiros dígitos")
+        cpf = input("XXX")
+        if len(cpf) == 8:
+            for x in range(100):
+                calcularCpfSecret(cpf)
+                if x == 99:
+                    print("CPF ", x ,": ",calcularCpfSecret(cpf))
+                    historico = []
+                
+            
+        else:
+            print("entrada inválida")
+
     controle = int(input("Digite: 0 ==> Reiniciar Programa || 1 ==> encerrar\n"))
     if controle != 0:
         print("Fim do Programa!\n© Deivs")
